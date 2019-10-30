7cdf03c73572cd601f8fa3cdd49a0cca4b9145c0
SarvM <saravana.msi@outlook.com>
2019-10-30 06:10:13 +0530

    2 files modified, 1 added

---
+++ src/app/highlight/higlight.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHiglight]'
+})
+export class HiglightDirective {
+
+  @Input() highlightColor: string;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    //this.el.nativeElement.style.backgroundColor = 'yellow';
+    this.highlight('yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    //this.el.nativeElement.style.backgroundColor = 'red';
+    this.highlight('red');
+  }
+
+  constructor(private el: ElementRef) { 
+    //el.nativeElement.style.backgroundColor = 'green';
+    this.highlight('green');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = this.highlightColor;
+  }
+
+}
